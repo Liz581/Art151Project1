b37bdc10b51b2d64f658f660f7d0ad8304436865
lrico <lrico5@uic.edu>
2021-09-29 08:14:17 -0500

    Final

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
+class Shapes {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  show() {
+    stroke(2);
+    square(this.x, this.y, 100);
+  }
+}
+
+let arr2 = [];
 function setup() {
-    createCanvas(600, 400);
+  createCanvas(1000, 1000);
+  alert("Art151: Project 1 \nCreated by Lisset Rico");
+  for (let i=0; i < 10; i++) {
+    arr2[i] = [];
+    for (let j=0; j < 100; j++) {
+      arr2[i][j] = new Shapes(i*100, j);
+    }
+  }
+}
+
+function draw() {
+  background(255, 211, 130);
+  strokeWeight(0.5);
+
+  for(let i=0; i < arr2.length; i++) {
+    for(let j=0; j < arr2[i].length; j++) {
+      arr2[i][j].show();
+    }
   }
   
-  function draw() {
-    background(0, 255, 220);
-  }
+}
